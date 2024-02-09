ff2c505ea01bd34a43a23d0a4b2eb3c572c7d68d
josue-cap <Josue.alvarado.cap@gmail.com>
2024-02-09 15:39:18 -0600

    my first commit on react and spring

---
+++ springboot-and-react/src/main/java/net/javaS/R/springbootandreact/SpringbootAndReactApplication.java
@@ -0,0 +1,13 @@
+package net.javaS.R.springbootandreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAndReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAndReactApplication.class, args);
+	}
+
+}
---
+++ springboot-and-react/src/test/java/net/javaS/R/springbootandreact/SpringbootAndReactApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaS.R.springbootandreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAndReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
