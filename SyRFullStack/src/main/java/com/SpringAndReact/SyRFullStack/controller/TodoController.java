fc8d573460c5557b131065f6a5b50004f6ef24e5
josue-cap <Josue.alvarado.cap@gmail.com>
2024-03-18 21:15:28 -0600

    adding modelMapper from the web

--- SyRFullStack/src/main/java/com/SpringAndReact/SyRFullStack/controller/TodoController.java
+++ SyRFullStack/src/main/java/com/SpringAndReact/SyRFullStack/controller/TodoController.java
@@ -1,4 +1,25 @@
 package com.SpringAndReact.SyRFullStack.controller;
 
+import com.SpringAndReact.SyRFullStack.dto.TodoDto;
+import com.SpringAndReact.SyRFullStack.service.TodoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/todos")
+@AllArgsConstructor
 public class TodoController {
+    private TodoService todoService;
+    //Build Add todo REST API
+     @PostMapping
+    public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto){
+        TodoDto savedDto = todoService.addTodo(todoDto);
+        return new ResponseEntity<>(savedDto, HttpStatus.CREATED);
+    }
+
 }
--- SyRFullStack/src/main/java/com/SpringAndReact/SyRFullStack/service/impl/TodoServiceImpl.java
+++ SyRFullStack/src/main/java/com/SpringAndReact/SyRFullStack/service/impl/TodoServiceImpl.java
@@ -5,28 +5,32 @@ import com.SpringAndReact.SyRFullStack.entity.Todo;
 import com.SpringAndReact.SyRFullStack.repository.TodoRepository;
 import com.SpringAndReact.SyRFullStack.service.TodoService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class TodoServiceImpl implements TodoService {
     private TodoRepository todoRepository;
+    private ModelMapper modelMapper;
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
         //Convert TodoDto into Todo Jpa entity
         //Todo Jpa entity
-        Todo todo = new Todo();
-        todo.setTitle(todoDto.getTitle());
-        todo.setDescription(todoDto.getDescription());
-        todo.setCompleted(todo.isCompleted());
+       // Todo todo = new Todo();
+        //todo.setTitle(todoDto.getTitle());
+        //todo.setDescription(todoDto.getDescription());
+        //todo.setCompleted(todo.isCompleted());
+        Todo todo = modelMapper.map(todoDto, Todo.class);
         // Todo Jpa entity
         Todo savedTodo = todoRepository.save(todo);
-        //convert saved Todo Jpa entity object into todoDto object
-        TodoDto savedTodoDto = new TodoDto();
-        savedTodoDto.setId(savedTodo.getId());
-        savedTodoDto.setTitle(savedTodo.getTitle());
-        savedTodoDto.setDescription(savedTodo.getDescription());
-        savedTodoDto.setCompleted(savedTodo.isCompleted());
+       /* //convert saved Todo Jpa entity object into todoDto object
+        // TodoDto savedTodoDto = new TodoDto();
+        // savedTodoDto.setId(savedTodo.getId());
+        //savedTodoDto.setTitle(savedTodo.getTitle());
+        //savedTodoDto.setDescription(savedTodo.getDescription());
+        //savedTodoDto.setCompleted(savedTodo.isCompleted()); */
+        TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
         return savedTodoDto;
     }
 }
