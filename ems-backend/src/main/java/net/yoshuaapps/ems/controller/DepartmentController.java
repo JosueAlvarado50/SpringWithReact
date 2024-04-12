8f6c200903fcd0faa7b542860528f0877d812456
josue-cap <Josue.alvarado.cap@gmail.com>
2024-04-12 11:18:07 -0600

    adding Method Level Security, and start with Roles from JPA entities

--- ems-backend/src/main/java/net/yoshuaapps/ems/controller/DepartmentController.java
+++ ems-backend/src/main/java/net/yoshuaapps/ems/controller/DepartmentController.java
@@ -6,6 +6,7 @@ import net.yoshuaapps.ems.dto.DepartmentDto;
 import net.yoshuaapps.ems.service.DepartmentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,30 +20,35 @@ public class DepartmentController {
 
     //Build Department REST API
     //CREATE DEPARTMENT
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<DepartmentDto> createDepartment(@RequestBody DepartmentDto departmentDto){
         DepartmentDto departmentCreated = departmentService.createDepartment(departmentDto);
         return new ResponseEntity<>(departmentCreated, HttpStatus.CREATED);
     }
     //GET DEPARTMENT BY ID
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping("{id}")
     public ResponseEntity<DepartmentDto> getDepartmentById(@PathVariable("id") Long departmentId){
         DepartmentDto departmentDto = departmentService.getDepartmentById(departmentId);
         return  ResponseEntity.ok(departmentDto);
     }
     //GET AlL DEPARTMENTS
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping
     public ResponseEntity<List<DepartmentDto>> getAllDepartments(){
         List<DepartmentDto> departmentDtoList = departmentService.getAllDepartments();
         return  ResponseEntity.ok(departmentDtoList);
     }
     //UPDATE DEPARTMENT
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
     public  ResponseEntity<DepartmentDto> updateDepartment(@PathVariable("id") Long departmentId,  @RequestBody DepartmentDto UpdatedDepartment){
         DepartmentDto departmentUpdated = departmentService.updateDepartment(departmentId, UpdatedDepartment);
         return ResponseEntity.ok(departmentUpdated);
     }
     //DELETE DEPARTMENT
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteDepartmentById(@PathVariable("id") Long departmentId){
         departmentService.deleteDepartmentById(departmentId);
--- ems-backend/src/main/java/net/yoshuaapps/ems/controller/EmployeeController.java
+++ ems-backend/src/main/java/net/yoshuaapps/ems/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import net.yoshuaapps.ems.dto.EmployeeDto;
 import net.yoshuaapps.ems.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
 @RequestMapping("/api/employees")
 public class EmployeeController {
     private EmployeeService employeeService;
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     // Build and Employee REST API
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
@@ -22,24 +24,27 @@ public class EmployeeController {
        return  new ResponseEntity<>(saveEmployee, HttpStatus.CREATED);
     }
     // Build Get employee REST API
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping("{id}")
     public  ResponseEntity <EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
         EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
         return  ResponseEntity.ok(employeeDto);
     }
     //Get all employees REST API
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping
     public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
         List<EmployeeDto> employeeDtoList = employeeService.getAllEmployees();
         return ResponseEntity.ok(employeeDtoList);
     }
     //Build updated employee REST API
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
     public  ResponseEntity<EmployeeDto> updateEmployeById(@PathVariable("id") Long employeeId, @RequestBody EmployeeDto updatedEmployee){
         EmployeeDto employeeDto = employeeService.updateEmployeById(employeeId, updatedEmployee);
         return ResponseEntity.ok(employeeDto);
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public  ResponseEntity<String> deleteEmployeeById(@PathVariable("id") Long employeeId){
         employeeService.deleteEmployeeById(employeeId);
