6220bac779b53f1c03e1eff935c4841b020f29f1
josue-cap <Josue.alvarado.cap@gmail.com>
2024-06-08 00:06:53 -0600

    Jwt auth response, role added to JwtAuthResponse, adding roles to AuthServiceImpl, switch String for JwtAuthResponse type of login service methd

--- SyRFullStack/src/main/java/com/SpringAndReact/SyRFullStack/service/AuthService.java
+++ SyRFullStack/src/main/java/com/SpringAndReact/SyRFullStack/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.SpringAndReact.SyRFullStack.service;
 
+import com.SpringAndReact.SyRFullStack.dto.JwtAuthResponse;
 import com.SpringAndReact.SyRFullStack.dto.LoginDto;
 import com.SpringAndReact.SyRFullStack.dto.RegisterDto;
 import org.springframework.stereotype.Service;
@@ -7,5 +8,5 @@ import org.springframework.stereotype.Service;
 @Service
 public interface AuthService {
     String register(RegisterDto registerDto);
-    String login(LoginDto loginDto);
+    JwtAuthResponse login(LoginDto loginDto);
 }
--- SyRFullStack/src/main/java/com/SpringAndReact/SyRFullStack/service/impl/AuthServiceImpl.java
+++ SyRFullStack/src/main/java/com/SpringAndReact/SyRFullStack/service/impl/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package com.SpringAndReact.SyRFullStack.service.impl;
 
+import com.SpringAndReact.SyRFullStack.dto.JwtAuthResponse;
 import com.SpringAndReact.SyRFullStack.dto.LoginDto;
 import com.SpringAndReact.SyRFullStack.dto.RegisterDto;
 import com.SpringAndReact.SyRFullStack.entity.Role;
@@ -19,6 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -63,13 +65,28 @@ public class AuthServiceImpl implements AuthService  {
     }
 
     @Override
-    public String login(LoginDto loginDto) {
+    public JwtAuthResponse login(LoginDto loginDto) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 loginDto.getUsernameOrEmail(),
                 loginDto.getPassword()
         ));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return jwtTokenProvider.generateToken(authentication);
+        String token = jwtTokenProvider.generateToken(authentication);
+        Optional<User> userOptional = userRepository.findByUsernameOrEmail(loginDto.getUsernameOrEmail(),
+                loginDto.getUsernameOrEmail());
+        String role = null;
+        if (userOptional.isPresent()){
+            User loggedInUser = userOptional.get();
+            Optional<Role> optionalRole = loggedInUser.getRoles().stream().findFirst();
+            if (optionalRole.isPresent()){
+                Role userRole = optionalRole.get();
+                role = userRole.getName();
+            }
+        }
+        JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
+        jwtAuthResponse.setRole(role);
+        jwtAuthResponse.setAccessToken(token);
+        return jwtAuthResponse;
 
     }
 
