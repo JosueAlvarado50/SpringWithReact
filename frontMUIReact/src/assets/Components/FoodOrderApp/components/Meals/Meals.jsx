7d4af6f0ef70aaf47720bbf85e47a01a05bcf63a
josue-cap <Josue.alvarado.cap@gmail.com>
2024-06-08 19:15:00 -0600

    adding Meals component to homePage of Food app, next lets continue with entity for DB with spring boot

--- frontMUIReact/src/assets/Components/FoodOrderApp/components/Meals/Meals.jsx
+++ frontMUIReact/src/assets/Components/FoodOrderApp/components/Meals/Meals.jsx
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
 
 const Meals = (props) => {
   return (
     <Fragment>
       <MealsSummary></MealsSummary>
+      <AvailableMeals onShowCart={props.onShowCart}></AvailableMeals>
     </Fragment>
   );
 };
