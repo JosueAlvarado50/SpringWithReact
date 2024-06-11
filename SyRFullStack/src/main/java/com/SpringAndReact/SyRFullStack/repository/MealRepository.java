0874688bcdde969a289543d0c5eb1b293f565967
josue-cap <Josue.alvarado.cap@gmail.com>
2024-06-11 14:32:59 -0600

    CRUD Client added

--- SyRFullStack/src/main/java/com/SpringAndReact/SyRFullStack/repository/MealRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SpringAndReact.SyRFullStack.repository;
-
-import com.SpringAndReact.SyRFullStack.entity.Meal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MealRepository  extends JpaRepository<Meal, Long> {
-}
--- SyRFullStack/src/main/java/com/SpringAndReact/SyRFullStack/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SpringAndReact.SyRFullStack.repository;
-
-import com.SpringAndReact.SyRFullStack.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
