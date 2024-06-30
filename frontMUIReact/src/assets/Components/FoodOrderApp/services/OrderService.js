f4dce856f8f662fb5f4307c2e32c34ae8d776b3a
josue-cap <Josue.alvarado.cap@gmail.com>
2024-06-30 17:12:21 -0600

    adding orderservice

---
+++ frontMUIReact/src/assets/Components/FoodOrderApp/services/OrderService.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+const REST_API_BASE_URL = "http://localhost:8081/api/food-order-app/order";
+
+const headers = {
+  Authorization: localStorage.getItem("token"),
+  "Content-Type": "application/json", // Asegúrate de establecer el tipo de contenido adecuado si es necesario
+};
+export const OrderList = () => {
+  return axios.get(REST_API_BASE_URL, { headers: headers });
+};
+
+export const getOrderById = (id) => {
+  return axios.get(REST_API_BASE_URL + "/" + id, {
+    headers: headers,
+  });
+};
+
+export const createOrder = (order) => {
+  return axios.post(REST_API_BASE_URL, order, {
+    headers: headers,
+  });
+};
+
+export const updateOrder = (id, order) => {
+  return axios.put(REST_API_BASE_URL + "/" + id, order, {
+    headers: headers,
+  });
+};
+
+export const deleteOrder = (id) => {
+  return axios.delete(REST_API_BASE_URL + "/" + id, {
+    headers: headers,
+  });
+};
